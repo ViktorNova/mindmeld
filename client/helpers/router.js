7e6264140deaa580c5a9f4325881a1d80ac9cac6
dcw303 <daniel.crowley.wilson@gmail.com>
2013-06-17 19:51:36 +0900

    refactored router functions into common logic

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,110 +1,85 @@
-Meteor.Router.add({
-  '/': {
-    to: 'home',
-    and: function() {
+function setSessionTeam(teamCode) {
+  var team = Teams.findOne({code: teamCode});
+  if (team) {
+    Session.set('currentTeamId', team._id);
+  }
+};
+
+function setSessionTeamAndProject(teamCode, projectCode) {
+  var team = Teams.findOne({code: teamCode});
+  if (team) {
+    Session.set('currentTeamId', team._id);
+    var project = Projects.findOne({teamId: team._id, code: projectCode});
+    if (project) {
+      Session.set('currentProjectId', project._id);
     }
-  },
-  '/:teamCode': {
-    to: 'team',
-    and: function(teamCode) { 
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
+  }
+};
+
+function setSessionTeamAndProjectAndMilestone(teamCode, projectCode, milestoneCode) {
+  var team = Teams.findOne({code: teamCode});
+  if (team) {
+    Session.set('currentTeamId', team._id);
+    var project = Projects.findOne({teamId: team._id, code: projectCode});
+    if (project) {
+      Session.set('currentProjectId', project._id);
+      var milestone = Milestones.findOne({teamId: team._id, 
+        projectId: project._id, code: milestoneCode});
+      if (milestone) {
+        Session.set('currentMilestoneId', milestone._id);
       }
     }
-  },
-  '/:teamCode/project/create': {
-    to: 'createProject',
-    and: function(teamCode) {
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-      }
-    }
-  },
-  '/:teamCode/:projectCode': {
-    to: 'project',
-    and: function(teamCode, projectCode) { 
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-        var project = Projects.findOne({teamId: team._id, code: projectCode});
-        if (project) {
-          Session.set('currentProjectId', project._id);
-        }
-      }
-    }
-  },
-  '/:teamCode/:projectCode/milestone/create': {
-    to: 'createMilestone',
-    and: function(teamCode, projectCode) {
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-        var project = Projects.findOne({teamId: team._id, code: projectCode});
-        if (project) {
-          Session.set('currentProjectId', project._id);
-        }
-      }
-    }
-  },
-  '/:teamCode/:projectCode/:milestoneCode': {
-    to: 'milestone',
-    and: function(teamCode, projectCode, milestoneCode) {
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-        var project = Projects.findOne({teamId: team._id, code: projectCode});
-        if (project) {
-          Session.set('currentProjectId', project._id);
-          var milestone = Milestones.findOne({teamId: team._id, 
-            projectId: project._id, code: milestoneCode});
-          if (milestone) {
-            Session.set('currentMilestoneId', milestone._id);
-          }
-        }
-      }
-    }
-  },
-  '/:teamCode/:projectCode/:milestoneCode/:issueCode': {
-    to: 'issue',
-    and: function(teamCode, projectCode, milestoneCode, issueCode) {
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-        var project = Projects.findOne({teamId: team._id, code: projectCode});
-        if (project) {
-          Session.set('currentProjectId', project._id);
-          var milestone = Milestones.findOne({teamId: team._id, 
-            projectId: project._id, code: milestoneCode});
-          if (milestone) {
-            Session.set('currentMilestoneId', milestone._id);
-            var issue = Issues.findOne({teamId: team._id, projectId: project._id, 
-              milestoneId: milestone._id, code: issueCode });
-            if (issue) {
-              Session.set('currentIssueId', issue._id); 
-            }
-          }
-        }
-      }
-    }
-  },
-  '/:teamCode/:projectCode/:milestoneCode/issue/create': {
-    to: 'createIssue',
-    and: function(teamCode, projectCode, milestoneCode) {
-      var team = Teams.findOne({code: teamCode});
-      if (team) {
-        Session.set('currentTeamId', team._id);
-        var project = Projects.findOne({teamId: team._id, code: projectCode});
-        if (project) {
-          Session.set('currentProjectId', project._id);
-          var milestone = Milestones.findOne({teamId: team._id, 
-            projectId: project._id, code: milestoneCode});
-          if (milestone) {
-            Session.set('currentMilestoneId', milestone._id);
-          }
+  }
+};
+
+function setSessionTeamAndProjectAndMilestoneAndIssue(teamCode, projectCode, milestoneCode, issueCode) {
+  var team = Teams.findOne({code: teamCode});
+  if (team) {
+    Session.set('currentTeamId', team._id);
+    var project = Projects.findOne({teamId: team._id, code: projectCode});
+    if (project) {
+      Session.set('currentProjectId', project._id);
+      var milestone = Milestones.findOne({teamId: team._id, 
+        projectId: project._id, code: milestoneCode});
+      if (milestone) {
+        Session.set('currentMilestoneId', milestone._id);
+        var issue = Issues.findOne({teamId: team._id, projectId: project._id, 
+          milestoneId: milestone._id, code: issueCode });
+        if (issue) {
+          Session.set('currentIssueId', issue._id); 
         }
       }
     }
   }
+}
+
+Meteor.Router.add({
+  '/': 
+    { to: 'home' },
+
+  '/:teamCode': 
+    { to: 'team', and: setSessionTeam },
+  '/:teamCode/edit': 
+    { to: 'editTeam', and: setSessionTeam },
+  '/:teamCode/project/create': 
+    { to: 'createProject', and: setSessionTeam },
+
+  '/:teamCode/:projectCode': 
+    { to: 'project', and: setSessionTeamAndProject },
+  '/:teamCode/:projectCode/edit':
+    { to: 'editProject', and: setSessionTeamAndProject },
+  '/:teamCode/:projectCode/milestone/create': 
+    { to: 'createMilestone', and: setSessionTeamAndProject },
+
+  '/:teamCode/:projectCode/:milestoneCode': 
+    { to: 'milestone', and: setSessionTeamAndProjectAndMilestone },
+  '/:teamCode/:projectCode/:milestoneCode/edit':
+    { to: 'editMilestone', and: setSessionTeamAndProjectAndMilestone },
+  '/:teamCode/:projectCode/:milestoneCode/issue/create': 
+    { to: 'createIssue', and: setSessionTeamAndProjectAndMilestone },
+
+  '/:teamCode/:projectCode/:milestoneCode/:issueCode': 
+    { to: 'issue', and: setSessionTeamAndProjectAndMilestoneAndIssue },
+  '/:teamCode/:projectCode/:milestoneCode/:issueCode/edit':
+    { to: 'editIssue', and: setSessionTeamAndProjectAndMilestoneAndIssue }
 });
