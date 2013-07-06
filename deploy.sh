b37a60240c7a2be3a32e8af20c61acbb2cf53790
dcw303 <daniel.crowley.wilson@gmail.com>
2013-07-06 15:15:24 +0900

    nodejitsu deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# forked from http://www.meteorpedia.com/read/Nodejitsu
+# original credit to Gadi Cohen
+# GPL license
+   
+rm -rf ~/tmp/meteorizer 
+demeteorizer -o ~/tmp/meteorizer
+cd ~/tmp/meteorizer
+npm install
+npm shrinkwrap
+ 
+# confirmed to work with output of demeteorizer 0.2.1
+tail -r package.json | tail -n 2 | tail -r  > package2.json
+cat >> package2.json << __END__
+  },
+  "subdomain": "mindmeld",
+  "domains": [
+    "mindmeld.io",
+    "www.mindmeld.io"
+  ],
+  "scripts": {
+    "start": "node server/server.js"
+  }
+}
+__END__
+mv package2.json package.json
+ 
+# Actual deploy to Nodejitsu
+jitsu deploy
