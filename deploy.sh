f85d0fdbdc266da125f47a1a11b2fa8335fc899f
dcw303 <daniel.crowley.wilson@gmail.com>
2013-07-07 11:48:50 +0900

    changing color scheme in logo

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,27 @@
 # original credit to Gadi Cohen
 # GPL license
    
-rm -rf ~/tmp/meteorizer 
-demeteorizer -o ~/tmp/meteorizer
-cd ~/tmp/meteorizer
+rm -rf ~/tmp/demeteorizer/
+demeteorizer -o ~/tmp/demeteorizer
+cd ~/tmp/demeteorizer
 npm install
 npm shrinkwrap
  
 # confirmed to work with output of demeteorizer 0.2.1
-tail -r package.json | tail -n 2 | tail -r  > package2.json
-cat >> package2.json << __END__
-  },
-  "subdomain": "mindmeld",
-  "domains": [
-    "mindmeld.io",
-    "www.mindmeld.io"
-  ],
-  "scripts": {
-    "start": "node server/server.js"
-  }
-}
-__END__
-mv package2.json package.json
+#tail -r package.json | tail -n 2 | tail -r  > package2.json
+#cat >> package2.json << __END__
+#  },
+#  "subdomain": "mindmeld",
+#  "domains": [
+#    "mindmeld.io",
+#    "www.mindmeld.io"
+#  ],
+#  "scripts": {
+#    "start": "node server/server.js"
+#  }
+#}
+#__END__
+#mv package2.json package.json
  
 # Actual deploy to Nodejitsu
-jitsu deploy
+#jitsu deploy
