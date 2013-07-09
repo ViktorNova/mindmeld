553cade2402f590293522f1af37eb5b2d2c654d0
dcw303 <daniel.crowley.wilson@gmail.com>
2013-07-09 23:04:12 +0900

    sortable issues in project view

--- client/views/project.js
+++ client/views/project.js
@@ -1,6 +1,20 @@
 Template.project.helpers(Meteor.userFunctions);
 Template.projectBody.helpers(Meteor.userFunctions);
 
+Template.projectBody.rendered = function() {
+
+  $(document).ready(function() { 
+    $('#sortableIssueList').sortable({ 
+      axis: "y",
+      beforeStop: function(event, ui) {
+        var rankedIssueIds = $( "#sortableIssueList" ).sortable( "toArray" );
+        Meteor.call('reorderIssueRankings', rankedIssueIds, Meteor.userFunctions.currentTeam()._id, Meteor.userFunctions.currentProject()._id);
+      }
+    });
+    $('#sortableIssueList').disableSelection();
+  });
+};
+
 Template.projectBody.events({
   'click #createFeature': function(event) {
     event.preventDefault();
@@ -13,13 +27,5 @@ Template.projectBody.events({
     Meteor.Router.to('editProject', 
       Meteor.userFunctions.teamCode.call(this), 
       this.code);
-  },
-  'click .increaseRank': function(event) {
-    event.preventDefault();
-    Meteor.call('increaseRank', event.toElement.dataset.team, event.toElement.dataset.project, event.toElement.dataset.issue);
-  },
-  'click .decreaseRank': function(event) {
-    event.preventDefault();
-    Meteor.call('decreaseRank', event.toElement.dataset.team, event.toElement.dataset.project, event.toElement.dataset.issue);
   }
 });
--- collections/issues.js
+++ collections/issues.js
@@ -229,11 +229,11 @@ Meteor.methods({
       }
     });
   },
+  reorderIssueRankings: function(issueIds, teamId, projectId) {
 
-  increaseRank: function(teamId, projectId, issueId) {
     var user = Meteor.user();
     if (!user)
-      throw new Meteor.Error(401, "You need to login to insert an issue in rankings");
+      throw new Meteor.Error(401, "You need to login to reorder issue rankings");
 
     if (!teamId)
       throw new Meteor.Error(500, "Issue did not have a teamId");
@@ -241,53 +241,11 @@ Meteor.methods({
     if (!projectId)
       throw new Meteor.Error(500, "Issue did not have a projectId");
 
-    if (!issueId)
-      throw new Meteor.Error(500, "Issue did not have an issueId");
+    if (!issueIds)
+      throw new Meteor.Error(500, "Issue Rankings were empty");
 
-    var issue = Issues.findOne({teamId: teamId, projectId: projectId, _id: issueId});
-    if (!issue)
-      throw new Meteor.Error(500, "No ranking matching that teamId, projectId and issueId found");
-
-    if (issue.rank == 1)
-      return;
-
-
-    var incumbent = Issues.findOne({teamId: teamId, projectId: projectId, rank: issue.rank - 1});
-    if (!incumbent)
-      return;
-
-    Issues.update({teamId: teamId, projectId: projectId, _id: incumbent._id},{$set: {rank: issue.rank }});
-    Issues.update({teamId: teamId, projectId: projectId, _id: issue._id}, {$set: {rank: issue.rank - 1 }});
-  },
-  decreaseRank: function(teamId, projectId, issueId) {
-    var user = Meteor.user();
-    if (!user)
-      throw new Meteor.Error(401, "You need to login to insert an issue in rankings");
-
-    if (!teamId)
-      throw new Meteor.Error(500, "Issue did not have a teamId");
-
-    if (!projectId)
-      throw new Meteor.Error(500, "Issue did not have a projectId");
-
-    if (!issueId)
-      throw new Meteor.Error(500, "Issue did not have an issueId");
-
-    var rankingIssueCount = Issues.find({teamId: teamId, projectId: projectId, rank: {$exists: true}}).count();
-
-    var issue = Issues.findOne({teamId: teamId, projectId: projectId, _id: issueId});
-    if (!issue)
-      throw new Meteor.Error(500, "No ranking matching that teamId, projectId and issueId found");
-
-    if (issue.rank == rankingIssueCount)
-      return;
-
-
-    var incumbent = Issues.findOne({teamId: teamId, projectId: projectId, rank: issue.rank + 1});
-    if (!incumbent)
-      return;
-
-    Issues.update({teamId: teamId, projectId: projectId, _id: incumbent._id},{$set: {rank: issue.rank }});
-    Issues.update({teamId: teamId, projectId: projectId, _id: issue._id}, {$set: {rank: issue.rank + 1 }});
+    _.each(issueIds, function(issueId, index) {
+      Issues.update({teamId: teamId, projectId: projectId, _id: issueId},{$set: {rank: index + 1}});
+    });
   }
 });
