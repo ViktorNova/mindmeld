8ee01a33348bd78c377a33966ac8f1a722efab13
dcw303 <daniel.crowley.wilson@gmail.com>
2013-07-15 23:27:10 +0900

    add timestamp to subscription

--- collections/subscriptions.js
+++ collections/subscriptions.js
@@ -1,8 +1,13 @@
 Subscriptions = new Meteor.Collection('subscriptions');
 
 Meteor.methods({
-  subscribe: function(subscription) {
-    if (!Subscriptions.findOne({email: subscription.email})) {
+  subscribe: function(subscriptionAttributes) {
+    if (!Subscriptions.findOne({email: subscriptionAttributes.email})) {
+      var subscription = _.extend(_.pick(subscriptionAttributes,
+        'name','email'), {
+        createdAt: new Date()
+      });
+
       Subscriptions.insert(subscription);
     }
   }
