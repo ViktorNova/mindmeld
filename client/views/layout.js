d8343a1279b22b7ffe1d7d49f0b6473402f16959
dcw303 <daniel.crowley.wilson@gmail.com>
2013-09-08 13:15:36 +0900

    uservoice integration

--- client/views/layout.js
+++ client/views/layout.js
@@ -1,5 +1,24 @@
 Template.layout.rendered = function() {
     new GoogleAnalytics('UA-43791015-1');
+
+    var uv=document.createElement('script');
+    uv.type='text/javascript';
+    uv.async=true;
+    uv.src='//widget.uservoice.com/F1y4PGuCWDka3zEzhqFNeQ.js';
+    var s=document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(uv,s);
+
+    UserVoice = window.UserVoice || [];
+    UserVoice.push(['showTab', 'classic_widget', {
+      mode: 'feedback',
+      primary_color: '#978057',
+      link_color: '#007dbf',
+      forum_id: 221775,
+      tab_label: 'Feedback',
+      tab_color: '#978057',
+      tab_position: 'middle-right',
+      tab_inverted: false
+    }]);
 }
  
 GoogleAnalytics = function(code) {
